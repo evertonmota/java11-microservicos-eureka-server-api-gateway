8d947f1e35d947897d2f2664720a7800ff5063ad
DESKTOP-M5PIO3V\Everton <mcostagt@gmail.com>
2023-11-13 14:13:20 -0300

    alteração do package

--- ms-worker/src/main/java/com/devsuperior/msworker/entities/Worker.java
+++ ms-worker/src/main/java/com/devsuperior/msworker/entities/Worker.java
@@ -1,4 +1,4 @@
-package com.devsuperior.hrworker.entities;
+package com.devsuperior.msworker.entities;
 
 import java.io.Serializable;
 import java.util.Objects;
--- ms-worker/src/main/java/com/devsuperior/msworker/repositories/WorkerRepository.java
+++ ms-worker/src/main/java/com/devsuperior/msworker/repositories/WorkerRepository.java
@@ -1,8 +1,8 @@
-package com.devsuperior.hrworker.repositories;
+package com.devsuperior.msworker.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.devsuperior.hrworker.entities.Worker;
+import com.devsuperior.msworker.entities.Worker;
 
 public interface WorkerRepository extends JpaRepository<Worker,Long>{
 
