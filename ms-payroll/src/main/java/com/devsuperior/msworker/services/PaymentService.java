0ff5b5b7389a278eb83e118412c00a5f49136961
DESKTOP-M5PIO3V\Everton <mcostagt@gmail.com>
2023-11-13 16:37:40 -0300

    Implementado RestTemplate - Comunicação entre dois Projetos

--- ms-payroll/src/main/java/com/devsuperior/msworker/services/PaymentService.java
+++ ms-payroll/src/main/java/com/devsuperior/msworker/services/PaymentService.java
@@ -1,13 +1,32 @@
 package com.devsuperior.msworker.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.devsuperior.msworker.entities.Payment;
+import com.devsuperior.msworker.entities.Worker;
 
 @Service
 public class PaymentService {
 
-	public Payment getPayment(Long workerId, int days) {
-		return new Payment("Bob", 200.0, days);
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Value("${ms-worker.host}")
+	private String msWorkerHost;
+	
+	public Payment getPayment(long workerId, int days) {
+		
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", ""+workerId );
+		
+		Worker worker = restTemplate.getForObject(msWorkerHost + "/workers/{id}", Worker.class, uriVariables);
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
