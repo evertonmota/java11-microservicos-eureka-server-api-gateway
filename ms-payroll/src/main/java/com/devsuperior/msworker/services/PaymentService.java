b5a254fbb1c2ae279e1f9c977fc35366d5f899c5
DESKTOP-M5PIO3V\Everton <mcostagt@gmail.com>
2023-11-13 21:50:49 -0300

    Adicionado o @FeignClient no lugar do RestTemplate

--- ms-payroll/src/main/java/com/devsuperior/msworker/services/PaymentService.java
+++ ms-payroll/src/main/java/com/devsuperior/msworker/services/PaymentService.java
@@ -1,32 +1,23 @@
 package com.devsuperior.msworker.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.devsuperior.msworker.entities.Payment;
 import com.devsuperior.msworker.entities.Worker;
+import com.devsuperior.msworker.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 
 	@Autowired
-	private RestTemplate restTemplate;
-	
-	@Value("${ms-worker.host}")
-	private String msWorkerHost;
-	
+	private WorkerFeignClient feignClient;
+
 	public Payment getPayment(long workerId, int days) {
-		
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workerId );
-		
-		Worker worker = restTemplate.getForObject(msWorkerHost + "/workers/{id}", Worker.class, uriVariables);
-		
-		return new Payment(worker.getName(), worker.getDailyIncome(), days);
+		Worker worker = feignClient.findById(workerId).getBody();
+		return new Payment(worker.getName(), worker.getDailyIncome(),days);
 	}
+
+	
+
 }
