17768b90326aac4482faa91fe5a5c24019db5e30
DESKTOP-M5PIO3V\Everton <mcostagt@gmail.com>
2023-11-15 12:31:23 -0300

    Configuração Zuul Api Gateway

--- ms-api-gateway-zuul/src/test/java/com/devsuperior/msapigatewayzuul/MsApiGatewayZuulApplicationTests.java
+++ ms-api-gateway-zuul/src/test/java/com/devsuperior/msapigatewayzuul/MsApiGatewayZuulApplicationTests.java
@@ -2,7 +2,11 @@ package com.devsuperior.msapigatewayzuul;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootTest
 class MsApiGatewayZuulApplicationTests {
 
