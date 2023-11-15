192f48c341414658c4378ffa96ef52b15935811e
DESKTOP-M5PIO3V\Everton <mcostagt@gmail.com>
2023-11-15 12:01:05 -0300

    Criando o Projeto Api Gateway Zuul

---
+++ ms-api-gateway-zuul/src/main/java/com/devsuperior/msapigatewayzuul/MsApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.msapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsApiGatewayZuulApplication.class, args);
+	}
+
+}
---
+++ ms-api-gateway-zuul/src/test/java/com/devsuperior/msapigatewayzuul/MsApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.msapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
