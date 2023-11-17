cc95f6b881bad9d56bcc888bbdc52e2f8f6f0a3e
DESKTOP-M5PIO3V\Everton <mcostagt@gmail.com>
2023-11-17 09:39:44 -0300

    Database e Bean de Configuração

--- ms-user/src/main/java/com/devsuperior/msuser/MsUserApplication.java
+++ ms-user/src/main/java/com/devsuperior/msuser/MsUserApplication.java
@@ -1,15 +1,27 @@
 package com.devsuperior.msuser;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class MsUserApplication {
+public class MsUserApplication implements CommandLineRunner{
 
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MsUserApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("BCRIPT = " + bCryptPasswordEncoder.encode("123456"));
+		
 	}
 
 }
